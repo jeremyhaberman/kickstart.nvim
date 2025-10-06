f8b1d393e3f3d8498bf4c2ccc23632ce37feed19
Jeremy Haberman <jhaberman@auditminer.com>
2025-10-06 07:00:54 -0500

    Use neo-tree instead of nvim-tree

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,42 +3,42 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  {
-    'nvim-tree/nvim-tree.lua',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = function()
-      -- Disable netrw (Vim's built-in file explorer)
-      vim.g.loaded_netrw = 1
-      vim.g.loaded_netrwPlugin = 1
-
-      require('nvim-tree').setup {
-        view = {
-          width = 40,
-          side = 'left',
-          number = true,
-          relativenumber = true,
-        },
-        renderer = {
-          group_empty = true, -- Collapse empty folders
-          icons = {
-            show = {
-              git = false,
-              file = false,
-              folder = false,
-              folder_arrow = false,
-            },
-          },
-          special_files = {},
-        },
-        filters = {
-          dotfiles = false, -- Show .env, .gitignore, etc.
-        },
-        git = {
-          ignore = false, -- Show files in .gitignore
-        },
-      }
-
-      vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle file explorer' })
-    end,
-  },
+  -- {
+  --   'nvim-tree/nvim-tree.lua',
+  --   dependencies = { 'nvim-tree/nvim-web-devicons' },
+  --   config = function()
+  --     -- Disable netrw (Vim's built-in file explorer)
+  --     vim.g.loaded_netrw = 1
+  --     vim.g.loaded_netrwPlugin = 1
+  --
+  --     require('nvim-tree').setup {
+  --       view = {
+  --         width = 40,
+  --         side = 'left',
+  --         number = true,
+  --         relativenumber = true,
+  --       },
+  --       renderer = {
+  --         group_empty = true, -- Collapse empty folders
+  --         icons = {
+  --           show = {
+  --             git = false,
+  --             file = false,
+  --             folder = false,
+  --             folder_arrow = false,
+  --           },
+  --         },
+  --         special_files = {},
+  --       },
+  --       filters = {
+  --         dotfiles = false, -- Show .env, .gitignore, etc.
+  --       },
+  --       git = {
+  --         ignore = false, -- Show files in .gitignore
+  --       },
+  --     }
+  --
+  --     vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle file explorer' })
+  --   end,
+  -- },
 }
--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -21,5 +21,15 @@ return {
         },
       },
     },
+    event_handlers = {
+      {
+        event = 'neo_tree_buffer_enter',
+        handler = function(arg)
+          vim.cmd [[
+          setlocal relativenumber
+        ]]
+        end,
+      },
+    },
   },
 }
