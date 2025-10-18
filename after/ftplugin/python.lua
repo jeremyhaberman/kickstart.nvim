22c28f2523d18fbf0ffb3e0dde0e630b5255a1c5
Jeremy Haberman <jhaberman@auditminer.com>
2025-10-18 10:37:36 -0500

    Remove preferred text width

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,4 +1,4 @@
-vim.opt_local.textwidth = 79 -- or your preferred width
+-- vim.opt_local.textwidth = 79 -- or your preferred width
 vim.opt_local.formatoptions:append 'c' -- auto-wrap comments
 vim.opt_local.formatoptions:append 'r' -- continue comments on <Enter>
 vim.opt_local.formatoptions:append 'o' -- continue comments on 'o' or 'O'
