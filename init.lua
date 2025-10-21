f1a6f2efe0afcdc239164e03ce4dd96e15105b97
Jeremy Haberman <jhaberman@auditminer.com>
2025-10-21 06:19:50 -0500

    Fix tabs and blink completion

--- init.lua
+++ init.lua
@@ -856,15 +856,19 @@ require('lazy').setup({
         --
         -- No, but seriously. Please read `:help ins-completion`, it is really good!
         --
-        -- All presets have the following mappings:
-        -- <tab>/<s-tab>: move to right/left of your snippet expansion
+        -- Default preset mappings:
+        -- <c-y>: Accept completion
         -- <c-space>: Open menu or open docs if already open
         -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
         -- <c-e>: Hide menu
         -- <c-k>: Toggle signature help
         --
+        -- Custom: <tab>/<s-tab> insert spaces (no snippet navigation)
+        --
         -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = 'default',
+        ['<Tab>'] = { 'fallback' },
+        ['<S-Tab>'] = { 'fallback' },
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
