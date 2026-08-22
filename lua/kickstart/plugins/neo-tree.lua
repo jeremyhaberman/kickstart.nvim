8ef104e6e62e70c8492bc9ab2f956fbd5a8a9e75
Jeremy Haberman <jhaberman@auditminer.com>
2026-08-22 06:34:35 -0500

    Don't open files in fugitive pane

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -14,6 +14,11 @@ return {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
+    -- When opening a file, neo-tree targets the most recently visited window
+    -- (see `prior_windows` in neo-tree.lua). Without this, coming from a
+    -- fugitive status/blame window makes the file replace *it* instead of the
+    -- editing window. This list replaces the defaults, so keep them.
+    open_files_do_not_replace_types = { 'terminal', 'Trouble', 'qf', 'edgy', 'fugitive', 'fugitiveblame' },
     filesystem = {
       window = {
         mappings = {
